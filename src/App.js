b5871751f0b6093b5f3e07bd38056a6a850d6011
azibzahid2017092 <73077300+azibzahid2017092@users.noreply.github.com>
2020-11-05 19:38:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Heroku Deployment By Azib Zahid 2017092!
         </p>
         <a
           className="App-link"
